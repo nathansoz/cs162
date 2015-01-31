cc945cd2ac04c597637c8724c46012f8f97cff29
Nathan Sosnovske <nathan@sosnovske.com>
2015-01-30 23:26:23 -0800

    Refactored main.cpp

--- labs/lab4/main.cpp
+++ labs/lab4/main.cpp
@@ -7,67 +7,86 @@
 #include "LoadedDice.h"
 #include "SozLibs/Stats.h"
 
+//controls the number of simulations that each dice pair gets
+const int NUM_SIM = 1000000;
 
 int rollTwoDice(const Dice& die1, const Dice& die2)
 {
     return die1.rollDice() + die2.rollDice();
 }
 
-
-int main()
+void diceSim(const Dice& die1, const Dice& die2, std::vector<int>& results, int numRolls)
 {
-    Dice die1;
-    Dice die2;
-
-    LoadedDice die3;
-    LoadedDice die4;
-
-    std::vector<int> unloadedSim;
-    std::vector<int> loadedSim;
-
-    std::cout << "Populating unloaded die:" << std::endl;
-    for(int i = 0; i < 1000000; i++)
+    for(int i = 0; i < numRolls; i++)
     {
         int result = rollTwoDice(die1, die2);
-        unloadedSim.push_back(result);
+        results.push_back(result);
 
     }
+}
 
-    std::cout << std::endl << "Populating loaded die:" << std::endl;
-
-    for(int i = 0; i < 1000000; i++)
-    {
-        int result = rollTwoDice(die3, die4);
-        loadedSim.push_back(result);
-    }
-
-    std::cout << "Mean of unloaded: " << SozLibs::Stats::Mean(unloadedSim) << std::endl;
-    std::cout << "Mean of loaded: " << SozLibs::Stats::Mean(loadedSim) << std::endl;
+void printStats(std::vector<int>& stats)
+{
+    std::cout << "Mean: " << SozLibs::Stats::Mean(stats) << std::endl;
 
     //median
 
-    std::cout << "Median of unloaded: " << SozLibs::Stats::Median(unloadedSim) << std::endl;
-    std::cout << "Median of loaded: " << SozLibs::Stats::Median(loadedSim) << std::endl;
+    std::cout << "Median: " << SozLibs::Stats::Median(stats) << std::endl;
 
     //mode of unloaded dice
-    std::vector<int> unloadedMode = SozLibs::Stats::Mode(unloadedSim);
-    std::cout << "Mode(s) of unloaded: ";
+    std::vector<int> unloadedMode = SozLibs::Stats::Mode(stats);
+    std::cout << "Mode: ";
     for(int i = 0; i < unloadedMode.size(); i++)
     {
         std::cout << unloadedMode.at(i) << " ";
     }
     std::cout << std::endl;
 
-    //mode of loaded dice
-    std::vector<int> loadedMode = SozLibs::Stats::Mode(loadedSim);
-    std::cout << "Mode(s) of loaded: ";
-    for(int i = 0; i < loadedMode.size(); i++)
-    {
-        std::cout << loadedMode.at(i) << " ";
-    }
-    std::cout << std::endl;
-
     //standard Deviation
-    std::cout << "Standard Deviation of unloaded: " << SozLibs::Stats::StdDev(unloadedSim) << std::endl;
-    std::cout << "Standard Deviation of loaded: " << SozLibs::Stats::StdDev(loadedSim) << std::endl;
+    std::cout << "Standard Deviation: " << SozLibs::Stats::StdDev(stats) << std::endl << std::endl;
+
+}
+
+int main()
+{
+    //standard 6 sided dice
+    Dice die1;
+    Dice die2;
+    LoadedDice die3;
+    LoadedDice die4;
+
+    //20 sided dice
+    Dice die5(20);
+    Dice die6(20);
+    LoadedDice die7(20);
+    LoadedDice die8(20);
+
+
+    std::vector<int> unloadedSimSix;
+    std::vector<int> loadedSimSix;
+    std::vector<int> unloadedSimTwenty;
+    std::vector<int> loadedSimTwenty;
+
+    std::cout << "Populating unloaded and loaded 6 sided dice rolls..." << std::endl << std::endl;
+
+    diceSim(die1, die2, unloadedSimSix, NUM_SIM);
+    diceSim(die3, die4, loadedSimSix, NUM_SIM);
+
+    //long running function calls
+    std::cout << "Unloaded, six sided dice stats: " << std::endl;
+    printStats(unloadedSimSix);
+    std::cout << "Loaded, six sided dice stats: " << std::endl;
+    printStats(loadedSimSix);
+
+    std::cout << "Populating unloaded and loaded 20 sided dice rolls..." << std::endl << std::endl;
+
+    diceSim(die5, die6, unloadedSimTwenty, NUM_SIM);
+    diceSim(die7, die8, loadedSimTwenty, NUM_SIM);
+
+    std::cout << "Unloaded, twenty sided dice stats: " << std::endl;
+    printStats(unloadedSimTwenty);
+    std::cout << "Loaded, twenty sided dice stats: " << std::endl;
+    printStats(loadedSimTwenty);
+
+
 }
