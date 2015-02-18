2f13a2c30c5298854298da96a86fc3e3e854574d
Nathan Sosnovske <nathan@sosnovske.com>
2015-02-17 22:21:09 -0800

    More corrections for flip

--- labs/lab7/generate.cpp
+++ labs/lab7/generate.cpp
@@ -1,5 +1,6 @@
 #include <boost/random/mersenne_twister.hpp>
-#include <boost/random/uniform_int_distribution.hpp>
+
+#include <boost/random/uniform_int.hpp>
 #include <boost/random/variate_generator.hpp>
 #include <boost/chrono.hpp>
 
@@ -10,14 +11,10 @@ void FillFile(std::ofstream &outFile, int numGenerations)
 {
     //Adapted from this example of c++11 random number generation:
     //  http://www.guyrutenberg.com/2014/05/03/c-mt19937-example/
+    boost::mt19937_64 randomNum(time(0));
+    boost::uniform_int<int> distribution(1, 9);
 
-    //std::mt19937_64 randomNum(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-    //std::uniform_int_distribution<int> distribution(1, 9);
-
-    boost::random::mt19937_64 randomNum(boost::chrono::high_resolution_clock::now().time_since_epoch().count());
-    boost::random::uniform_int_distribution<int> distribution(1, 9);
-
-    boost::random::variate_generator< boost::mt19937_64&, boost::random::uniform_int_distribution<int> >
+    boost::random::variate_generator< boost::mt19937_64&, boost::uniform_int<int> >
             generateRand( randomNum, distribution);
 
     if(outFile)
